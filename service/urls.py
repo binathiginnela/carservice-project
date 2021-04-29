1bb0268086e7ce0c0d086d8a15692d6a0a3fd23e
binati <binathiginnela@gmail.com>
2021-04-29 20:08:34 +0530

    pro-1

---
+++ service/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from service import views
+from django.contrib.auth import views as ad
+urlpatterns=[
+       path('',views.home,name="home"),
+       path('a/',views.about,name="ab"),
+       path('c/',views.contact,name="cn"),
+       path('log/',ad.LoginView.as_view(template_name="services/login.html"),name="lg"),
+       path('reg/',views.registration,name="reg"),
+       path('ser/',views.Service,name="ser"),
+       path('logout/',ad.LogoutView.as_view(template_name="services/logout.html"),name="lgo"),
+       path('profile/',views.profile,name='pro'),
+       path('e/<int:si>/',views.userupdate,name="ue"),
+       path('delt/<int:st>/',views.deletedata,name="delete"),
+       path('ch/',views.cgf,name="cg"),
+       path('showdata/',views.showinfo,name="show"),
+       path('infodelete/<int:et>',views.infodelete,name='infodelete'),
+       path('privacypolicy/',views.privacypolicy,name="pri"),
+       path('termsconditions/',views.termsconditions,name="ter"),
+       path('sustainability/',views.sustainability,name="su")
+      
+]
